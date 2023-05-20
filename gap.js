e3eb6888ec0db64ce14e8c43eaf749baf828b053
Jean <jeancpires09@gmail.com>
2023-05-20 01:38:25 -0300

    daily update

---
+++ gap.js
@@ -0,0 +1,15 @@
+// So this function should return the first pair of two prime numbers spaced with a gap of g 
+// between the limits m, n if these numbers exist otherwise `nil or null or None or Nothing
+//  (or ... depending on the language). 
+function gap(g, m, n) {
+   let interval = []
+   for (let i = 0; i < n; i++) {
+      interval[i] = m+i      
+   }
+   for (let i = m; i < n; i++) {
+      if (isPrime()) {
+         
+      }      
+   }
+
+}
--- scramble.js
+++ scramble.js
@@ -1,19 +1,24 @@
 function scramble(str1, str2) {
-    let a1 = str1.split("")
-    let a2 = str2.split("")
-    debugger
-    if (a2.length == 0) {
-        return true
-    } else{
-        for (let i = 0; i < a1.length; i++) {
-            if (a1[i] == a2[0]) {
-                a2.shift()
-                scramble(a1.join(""), a2.join(""))
-            }
-        }
+    const charCount1 = countCharacters(str1);
+    const charCount2 = countCharacters(str2);
+    for (let char in charCount2) {
+      if (!charCount1[char] || charCount1[char] < charCount2[char]) {
+        return false;
+      }
     }
-
-    return false;
-}
   
-console.log(scramble('rkqodlw', 'world'))
+    return true;
+  }
+  
+  function countCharacters(str) {
+    debugger
+    const charCount = {};
+  
+    for (let char of str) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+  
+    return charCount;
+  }
+  
+console.log(scramble('cedewaraaossoqqyt', 'codewars'))
