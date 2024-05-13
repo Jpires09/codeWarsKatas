0efbb814e4549d35c9eccdc2bcc093ece71130c2
Jpires09 <jeancpires09@gmail.com>
2024-05-13 20:45:49 -0300

    initial commit

---
+++ ruby/next_higher/next_higher.rb
@@ -0,0 +1,8 @@
+def next_higher(n):
+    
+end
+# Test cases
+print(next_higher(129))     # Output: 130
+print(next_higher(127))     # Output: 191
+print(next_higher(1))       # Output: 2
+print(next_higher(323423))  # Output: 323439
